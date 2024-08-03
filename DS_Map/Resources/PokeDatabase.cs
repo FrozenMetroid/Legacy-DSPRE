6ec61733d2eff84f9302f7844833f3665bfc252d
Senate <frozenmetroid7@gmail.com>
2024-08-03 01:12:18 -0400

    Update PokeDatabase.cs

--- DS_Map/Resources/PokeDatabase.cs
+++ DS_Map/Resources/PokeDatabase.cs
@@ -1496,6 +1496,7 @@ namespace DSPRE.Resources {
                     [new List<uint> { 821 }] = (Color.SteelBlue, Color.White),
                     [new List<uint> { 823 }] = (Color.ForestGreen, Color.White),
                     [new List<uint> { 824 }] = (Color.ForestGreen, Color.White),
+                    [new List<uint> { 825 }] = (Color.SteelBlue, Color.White),
                     [new List<uint> { GameMatrix.EMPTY }] = (Color.Black, Color.White)
                 };
                 public static Dictionary<List<uint>, (Color back, Color fore)> GenericMatrixColorsDict = new Dictionary<List<uint>, (Color back, Color fore)> {
